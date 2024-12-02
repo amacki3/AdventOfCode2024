1941c36d985507af1648c5d229da96e4cfc339f3
Andrew Mackie <andrew.mackie@in-space.co.uk>
2024-12-02 11:46:26 +0000

    Day 1 Solve 2

--- 01/solve.py
+++ 01/solve.py
@@ -1,12 +1,24 @@
 import numpy as np
 
+
+def read_data(inputfile):
+     data = np.loadtxt(inputfile,dtype=float).T
+     return data
+
 def solve_part_one(input_file_name="input.txt"):
-    data = np.loadtxt(input_file_name,dtype=float).T
+    data = read_data(input_file_name)
     data = np.sort(data,axis = 1)
     print(data)
     vals = data[1] - data[0]
     return np.sum(abs(vals))
     
+def solve_part_two(input_file_name="input.txt"):
+    data = read_data(input_file_name)
+    total = 0
+    for elem in data[0]:
+        total += elem * np.sum(data[1] == elem)
+    return total
 
 if __name__ == '__main__':
     print(solve_part_one())
+    print(solve_part_two())
